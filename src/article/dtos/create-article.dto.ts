8ac19f109b11c02d7342342205b1d93be257f159
Timo Ischen <info@timo-ischen.de>
2023-03-24 18:10:43 +0100

    fix: add doc annotations for article dto

--- src/article/dtos/create-article.dto.ts
+++ src/article/dtos/create-article.dto.ts
@@ -1,10 +1,19 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsNotEmpty, IsString } from 'class-validator';
 
 export class CreateArticleDto {
+  @ApiProperty({
+    description: 'The title of the article',
+    example: 'Hello World',
+  })
   @IsNotEmpty()
   @IsString()
   title: string;
 
+  @ApiProperty({
+    description: 'The content of the article',
+    example: 'This is my first article.',
+  })
   @IsNotEmpty()
   @IsString()
   content: string;
--- src/article/dtos/create-vote.dto.ts
+++ src/article/dtos/create-vote.dto.ts
@@ -1,11 +1,20 @@
 import { IsNotEmpty, IsString, IsEnum } from 'class-validator';
 import { VoteType } from '@prisma/client';
+import { ApiProperty } from '@nestjs/swagger';
 
 export class CreateVoteDto {
+  @ApiProperty({
+    description: 'The userId of the user',
+    example: '1234',
+  })
   @IsNotEmpty()
   @IsString()
   userId: string;
 
+  @ApiProperty({
+    description: 'The voteType of the vote',
+    example: 'UPVOTE',
+  })
   @IsNotEmpty()
   @IsEnum(VoteType)
   voteType: VoteType;
